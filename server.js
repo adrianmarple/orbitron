0c02190e1d5d0cbc90cbf3aa49cb8e383c3e9849
Mana <manadream@protonmail.com>
2022-08-30 15:59:28 -0700

    Added 404 response

--- server.js
+++ server.js
@@ -718,15 +718,12 @@ const rootServer = http.createServer(function (request, response) {
   fs.readFile(filePath, function(error, content) {
     if (error) {
       if(error.code == 'ENOENT'){
-        fs.readFile('./404.html', function(error, content) {
-          response.writeHead(200, { 'Content-Type': contentType })
-          response.end(content, 'utf-8')
-        });
+        response.writeHead(404)
+        response.end(`Nothing found at ${filePath}. Either the Orb is not connected or the URL is incorrect. Check the URL or refresh the page to try again.`, 'utf-8')
       }
       else {
         response.writeHead(500)
         response.end('Sorry, check with the site admin for error: '+error.code+' ..\n')
-        response.end();
       }
     }
     else {
