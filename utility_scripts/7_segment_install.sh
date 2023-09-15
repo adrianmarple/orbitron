f1500b317625b2e7d1f31ae05ced34a95106d2f9
Adrian Marple <adrian.marple@gmail.com>
2023-09-14 22:04:59 -0700

    Added install script for segmented display libraries

---
+++ utility_scripts/7_segment_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip3 install adafruit-circuitpython-ht16k33
+sudo pip3 install --upgrade adafruit-python-shell
+sudo apt-get update
+sudo apt-get install python3-pil
+sudo python3 raspi-blinka.py
+
