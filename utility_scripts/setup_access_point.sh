5295dbfd3753153713f75fb428e3b540e13d9999
manadream <manadream@protonmail.com>
2023-10-25 13:01:02 -0700

    Removed hostapd enble from install script

--- utility_scripts/setup_access_point.sh
+++ utility_scripts/setup_access_point.sh
@@ -1,6 +1,5 @@
 sudo apt install hostapd
 sudo systemctl unmask hostapd
-sudo systemctl enable hostapd
 sudo apt install dnsmasq
 sudo cp ~/orbitron/utility_scripts/dhcpcd.conf /etc/dhcpcd.conf
 sudo cp ~/orbitron/utility_scripts/dnsmasq.conf /etc
