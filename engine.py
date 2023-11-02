d82a7872db422c91a5a763223b577e1545b05298
Adrian Marple <adrian.marple@gmail.com>
2023-11-01 19:24:23 -0700

    time of day color change

--- engine.py
+++ engine.py
@@ -929,9 +929,9 @@ def rainbow_phase_color():
 tod_colors = [
   [0, np.array((0.6, 0.2, 0))],
   [0.2, np.array((0.6, 0.2, 0))],
-  [0.3, np.array((1.0, 1.0, 1.0))],
-  [0.2, np.array((1.0, 1.0, 1.0))],
-  [0.15, np.array((0.9, 0.7, 0.1))],
+  [0.3, np.array((0.7, 0.7, 0.7))],
+  [0.2, np.array((0.7, 0.7, 0.7))],
+  [0.15, np.array((0.7, 0.5, 0.1))],
   [0.15, np.array((0.6, 0.2, 0))],
 ]
 def time_of_day_color():
