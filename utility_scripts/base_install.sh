c577f435aa718cbea480e1779c52e4425b638a70
manadream <manadream@protonmail.com>
2023-11-26 14:49:50 -0800

    Fixed install script

--- utility_scripts/base_install.sh
+++ utility_scripts/base_install.sh
@@ -2,7 +2,7 @@
 
 sudo sh -c 'echo orbitron > /etc/hostname'
 sudo apt-get update
-sudo apt-get install python3-pip python3-numpy python3-scipy libsdl2-2.0 libsdl2-mixer-2.0-0 nodejs npm python3-websockets python3-pygame python3-venv python3-pil i2c-tools libgpiod-dev
+sudo apt-get install python3-pip python3-numpy python3-scipy libsdl2-2.0 libsdl2-mixer-2.0-0 nodejs npm python3-websockets python3-pygame python3-venv python3-pil i2c-tools libgpiod-dev vim
 
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_spi 0
@@ -12,9 +12,9 @@ sudo raspi-config nonint disable_raspi_config_at_boot 0
 
 sudo python -m venv --system-site-packages ~/.env
 source ~/.env/bin/activate
-pip3 install adafruit-circuitpython-neopixel adafruit-circuitpython-ht16k33 adafruit-python-shell setuptools RPi.GPIO adafruit-blinka
+pip3 install adafruit-circuitpython-neopixel adafruit-circuitpython-ht16k33 adafruit-python-shell setuptools RPi.GPIO adafruit-blinka rpi_ws281x
+deactivate
 cd ~/orbitron
 npm install
 cp config.js.template config.js
-deactivate
 echo "BASE INSTALL DONE!"
