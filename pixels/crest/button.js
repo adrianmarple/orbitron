e805802529336919a2b8ee173a67e93e2ccdffb3
Adrian Marple <adrian.marple@gmail.com>
2023-12-03 13:13:18 -0800

    Fixed byte order for external board.

--- pixels/crest/button.js
+++ pixels/crest/button.js
@@ -18,7 +18,7 @@ addButton("de Ruyter Crest", () => {
     CHANNEL_WIDTH: 5,
     CHANNEL_DEPTH: 8,
     NOTCH_DEPTH: 3,
-    PIXEL_DISTANCE: 8.333,
+    PIXEL_DISTANCE: 8.333, //https://www.superlightingled.com/4mm-ws2812c-individually-addressable-rgb-led-strip-light-120ledsm-328ft1m-p-4003.html
   })
 
   for (let i = 0; i < 4; i++) {
