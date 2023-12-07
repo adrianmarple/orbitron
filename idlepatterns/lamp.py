b0eb64e73dc408ab838c7e8744378bca79285ea0
Adrian Marple <adrian.marple@gmail.com>
2023-12-06 22:03:06 -0800

    Updated lamp.

--- idlepatterns/lamp.py
+++ idlepatterns/lamp.py
@@ -6,26 +6,13 @@ import engine
 from idlepatterns import Idle
 
 class Lamp(Idle):
-  def render(self):
-    if not self.wait_for_frame_end():
-      self.blend_pixels()
-      self.apply_color()
-      engine.raw_pixels = self.render_values * 255
-      return
-    self.init_values()
-    self.render_values *= 0.5
+
+  def init_values(self):
+    Idle.init_values(self)
     spotlight = np.matmul(-engine.coords[0], engine.unique_coord_matrix)
     spotlight = np.maximum(spotlight, 0)
     spotlight = np.multiply(spotlight, spotlight)
     self.render_values += spotlight
-    self.render_values = np.minimum(self.render_values, 1)
 
-    self.apply_min()
-    self.apply_color()
-    self.apply_brightness()
-
-    self.render_values *= 255
-    self.render_values = np.maximum(self.render_values, 1)
-    engine.raw_pixels = self.render_values
 
 idle = Lamp()
