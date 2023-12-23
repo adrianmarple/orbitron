744108b54bb99d4d97da2dd15681e5f23b35ce13
Adrian Marple <adrian.marple@gmail.com>
2023-12-23 15:48:08 -0800

    Minor 14 segment logging change.

--- orb.js
+++ orb.js
@@ -57,6 +57,7 @@ function connectOrbToServer(){
       if(data == "GIT_HAS_UPDATE"){
         console.log("Received notice of git update.")
         if (config.CONTINUOUS_INTEGRATION) {
+          displayText("RESTARTING")
           pullAndRestart()
         }
         return
