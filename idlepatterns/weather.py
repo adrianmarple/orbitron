bdb1ae536538a507cb72dc4b3670a5dde28eaef4
Adrian Marple <adrian.marple@gmail.com>
2024-01-20 17:16:10 -0800

    Got weather data idle working.

---
+++ idlepatterns/weather.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+import json
+from math import pi, sqrt, sin, cos
+import numpy as np
+import os
+from random import randrange, random
+import sys
+from time import time
+import urllib.request
+
+import engine
+from engine import RAW_SIZE, unique_coord_matrix
+from idlepatterns import Idle
+
+config = json.loads(os.getenv("CONFIG"))
+
+class Weather(Idle):
+  previous_update_time = 0
+  time_factor = 0
+  previous_time = 0
+
+  def update_weather_data(self):
+    url = "https://api.openweathermap.org/data/3.0/onecall?lat=%s&lon=%s&exclude=%s&appid=%s" % (
+      config["LAT"],
+      config["LON"],
+      "current,minutely,daily,alerts",
+      config["WEATHER_API_KEY"]
+    )
+    print(url, file=sys.stderr)
+    contents = urllib.request.urlopen(url).read()
+    self.weather_data = json.loads(contents)["hourly"]
+    self.previous_update_time = time()
+
+  def set_current_weather(self):
+    if time() - self.previous_update_time > 60*60:
+      self.update_weather_data()
+
+    index = len(self.weather_data)
+    for i in range(len(self.weather_data)):
+      if self.weather_data[i]["dt"] > time():
+        index = i
+        break
+
+    if index == 0:
+      index = 1
+      a = 0
+    elif index == len(self.weather_data):
+      index -= 1
+      a = 1
+    else:
+      delta_t = self.weather_data[index]["dt"] - self.weather_data[index - 1]["dt"]
+      a = (time() - self.weather_data[index - 1]["dt"]) / delta_t
+    
+    snapshot0 = self.weather_data[index - 1]
+    snapshot1 = self.weather_data[index]
+
+    self.wind = a * wind_vector(snapshot0) + (1-a) * wind_vector(snapshot1) # m/s
+    self.uvi = a * snapshot0["uvi"] + (1-a) * snapshot1["uvi"]
+    self.temp = a * snapshot0["temp"] + (1-a) * snapshot1["temp"] # Kelvin
+    self.rain = a * snapshot0["rain"]["1h"] + (1-a) * snapshot1["rain"]["1h"] # mm/h
+
+  def init_values(self):
+    self.set_current_weather()
+    self.render_values = np.matmul(self.wind, unique_coord_matrix)
+    self.time_factor += (time() - self.previous_time) * 2*pi / 20 * np.linalg.norm(self.wind)
+    self.previous_time = time()
+    self.render_values += self.time_factor
+    self.render_values = (np.sin(self.render_values) + 1.1)/2.1
+
+  def apply_color(self):
+    end = np.array((0, 0, 0.9))
+    end[1] += (self.temp - 275) / 50
+    start = np.array((0.1, 0.9, 0.1))
+    start[2] += sqrt(self.rain)/2
+
+    rectified_target_values = self.target_values * 1
+    rectified_target_values = np.minimum(1, rectified_target_values)
+    start_colors = np.outer(rectified_target_values, start)
+    end_colors = np.outer(1 - rectified_target_values, end)
+    colors = start_colors + end_colors
+
+    uvf = 1 - 1 / (1 + self.uvi)
+    uv_color = np.array((1 - 0.4*uvf, 1 - 0.7*uvf, 1 - 0.9*uvf))
+    self.render_values = np.outer(self.render_values, uv_color)
+    self.render_values = np.multiply(self.render_values, colors)
+
+  def color(self):
+    r = 1 / (1 + self.uvi)
+    b = sqrt(self.rain)
+    b = min(b, 1)
+    return np.array((r,g,b))
+
+def wind_vector(snapshot):
+  theta = snapshot["wind_deg"] * pi / 180 # clockwise from north
+  return np.array((sin(theta), cos(theta), 0)) * snapshot["wind_speed"]
+
+idle = Weather()
---
+++ pixels/family/rectangle.js
@@ -0,0 +1,16 @@
+
+addButton("Rectangle", () => {
+  name = "mieke"
+  setLaserParams({
+    BORDER: 4,
+    CHANNEL_WIDTH: 5,
+    CHANNEL_DEPTH: 8,
+    NOTCH_DEPTH: 3,
+    PIXEL_DISTANCE: 8.333, //https://www.superlightingled.com/4mm-ws2812c-individually-addressable-rgb-led-strip-light-120ledsm-328ft1m-p-4003.html
+  })
+  
+  addPolygon(4, [0,0,0], [60,90])
+  console.log(verticies.length)
+  
+  EulerianPath(0)
+})
