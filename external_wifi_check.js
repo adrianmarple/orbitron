32ae86ae5fa688de296917673e76a2e655a4232f
manadream <manadream@protonmail.com>
2025-05-20 18:59:53 -0700

    some more wifi script tweaks

--- external_wifi_check.js
+++ external_wifi_check.js
@@ -10,7 +10,7 @@ async function check_external_wifi(){
       console.log("USB WIFI showing as DISK, changing USB mode")
       let vendor_product = usbreset.split(" ").filter(function(data){ return data.includes(":") })[0].split(":")
       console.log(vendor_product)
-      await execute("usb_modeswitch -R -K -v " + vendor_product[0] + " -p " + vendor_product[1])
+      console.log((await execute(`usb_modeswitch -R -K -v ${vendor_product[0]} -p ${vendor_product[1]}`)))
       let failed = (await execute("usbreset | grep -i disk")).toLowerCase().includes("disk")
       if(failed){
         console.log("Failed to reset USB WIFI! Still showing as DISK.")
@@ -20,7 +20,7 @@ async function check_external_wifi(){
     }
 
     console.log("Checking if internal wifi is disabled")
-    let has_two_wifis = (await execute("ifconfig | grep -i wlan1")).includes("wlan1")
+    let has_two_wifis = (await execute("ifconfig")).includes("wlan1")
     if(has_two_wifis){
       console.log("Internal wifi is enabled")
       if(!blacklist_present){
