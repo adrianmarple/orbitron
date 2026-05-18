1304d0cc22b64d860e8a0859509eca4c02c74322
Adrian Marple <adrian.marple@gmail.com>
2026-05-18 15:51:36 -0700

    Figuring out potential large infinite mirror cube.

--- creation/projects/archimedes/cubetrue.js
+++ creation/projects/archimedes/cubetrue.js
@@ -1,16 +1,26 @@
+// v1.0.1
 module.exports = () => {
+  CHANNEL_DEPTH = 12
+  CHANNEL_WIDTH = 12
+  BORDER = 1.4
+
   buildArchimedean([
     [1,1,1],
   ], {
-    edgeLength: 6,
+    edgeLength: 26,
   })
 
   printPostProcessingFunction = printInfo => {
     bracketCapturePostProcessing({
-      indicies: {4: 0, "top": 2, "bottom": 3}
+      indicies: {4: 0, "top": 2, "bottom": 3},
+      glassROffset: -1,
     })(printInfo)
   }
 
+  for (let edge of [...edges]) {
+    splitEdge(edge, edge.length()/2)
+  }
+
   edgeCleanup()
-  EulerianPath(0)
+  EulerianPath(0, 1)
 }
